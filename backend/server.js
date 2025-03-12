6756b622e03179ba5d932437ee8af8f35e6ffa65
shivangChordia <shivangchordia02@gmail.com>
2025-03-12 19:57:49 -0400

    some config for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: allowedOrigins,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
 );
