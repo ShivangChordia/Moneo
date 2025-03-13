a20de9a30d04dc48603d0b57cd15e97d0480a5ff
shivangChordia <shivangchordia02@gmail.com>
2025-03-12 20:07:35 -0400

    some config for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
