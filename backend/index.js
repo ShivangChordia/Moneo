88bd467902298867291ae010ab8c3c9016aabfcb
KevalPatel2 <patelkeval212@gmail.com>
2025-04-21 13:16:38 -0400

    Update naming to index.js so vercel can expect it

--- backend/index.js
+++ backend/index.js
@@ -152,6 +152,4 @@ app.use("/api/purchase", purchaseRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
 module.exports = app;
